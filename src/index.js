18ad6168a671abc08bef50addc3c62869f8edb11
Justin Furtado <justin.l.furtado@gmail.com>
2017-08-11 09:01:02 -0600

    Temporary test cookie

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ var express = require('express'),
     pug = require('pug'),
     path = require('path'),
     // route = require('./routes/routes.js'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -10,29 +11,41 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser('The answer to the question of life, the universe, and everything'));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    var loggedIn = (req.cookies.c1 === 'true');
+    if (!loggedIn) {res.render('index', { title: 'Home' });}
+    else { res.render('index', {title: 'Home',  msg: "YOU LOGGED IN"}); }
+    
 });
 
 app.get('/register', (req, res) => {
+    res.cookie('c1', 'true');
     res.render('register', { title: 'Register' });
 });
+
 app.get('/login', (req, res) => {
+    res.cookie('c1', 'true');
     res.render('login', { title: 'Login' });
 });
+
 app.get('/profile', (req, res) => {
     res.render('profile', { title: 'Profile' });
 });
+
 app.get('/edit-profile', (req, res) => {
     res.render('edit-profile', { title: 'Edit Profile' });
 });
+
 app.get('/users', (req, res) => {
     res.render('users', { title: 'Users' });
 });
+
 app.get('/logout', (req, res) => {
+    res.clearCookie('c1');
     res.render('index', { title: 'Home', msg: 'Logout successful' });
 });
 
