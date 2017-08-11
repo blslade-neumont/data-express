beefed8baf1f9d109c6289961e792b6c438261ef
Brandon <brandonyoyoslade@gmail.com>
2017-08-11 08:24:45 -0600

    feat(*): add views, public, and initial express app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+var express = require('express'),
+    pug = require('pug'),
+    path = require('path'),
+    // route = require('./routes/routes.js'),
+    bodyParser = require('body-parser');
 
+var app = express();
 
-console.log('Hello, World!');
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+// app.get('/create', route.create);
+// app.get('/edit/:id', route.edit);
+// app.get('/details/:id', route.details);
+// app.post('/create', urlencodedParser, route.createPerson);
+// app.post('/edit/:id', urlencodedParser, route.editPerson);
+// app.get('/delete/:id', route.delete);
+
+app.listen(8080);
